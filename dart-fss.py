81e8798ba4abee77eddcb3609abfa2f0a98e41b5
oneum20 <63915349+oneum20@users.noreply.github.com>
2021-06-17 22:45:31 +0900

    Update dart-fss.py
    Convert DataFrame
     - 2 dimension index -> 1 dimension index
     - pivot table
     - configure index

--- dart-fss.py
+++ dart-fss.py
@@ -16,20 +16,31 @@ corps = [*corp_list._corp_codes]
 
 # Get Data
 print("[RUN] Getting Data...")
-fs = dart.fs.extract(corp_code='00126380', bgn_de='20170101', fs_tp=['bs'], report_tp='annual', lang='ko')
-print(fs['bs'])
+corp_code = '00126380'
+fs = dart.fs.extract(corp_code=corp_code bgn_de='20170101', fs_tp=['bs'], report_tp='annual', lang='ko')
+df = fs['bs']
 
-#===========================
-# DataFrame test
-#===========================
-fs2 = fs['bs']
+# Convert DataFrame
+## two dimension index -> one dimension index
+long_clm = df.columns[0][0]
+df.rename(columns={long_clm:"D"}, inplace=True) # Rename Long Columns
+level0 = list(df.columns.get_level_values(0)) # Get 0 level index
+level1 = list(df.columns.get_level_values(1)) # Get 1 level index
+level1[8:] = ['']*len(level1[8:])
 
-long_clm = fs2.columns[0][0]
-fs2.rename(columns={long_clm:"D"}, inplace=True)
+level = [x + y for x, y in zip(level0, level1)]
+df.columns = level # Columns
 
-level0 = fs2.columns.get_level_values(0)
-level1 = fs2.columns.get_level_values(1)
+## pivot table
+newCol = ['Dclass2'] + level[8:] # Set New Columns 
+tmp = df[newCol].pivot_table(columns = ['Dclass2'], values = level[8:], aggfunc='sum').reset_index() # Pivot Table
+tmp.rename(columns = {'index':'year'}, inplace = True) # Rename Colmuns
 
-fs2.columns = level0 + "_" + level1 # Type Error
+## add corp_code columns & swap index(corp_code <-> year)
+tmp.insert(0, 'corp_code', corp_code) # Insert corp_code
+tmp = tmp.set_index('corp_code') # Change index
 
-##========Test end======== 
+
+
+
+
