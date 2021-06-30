c68c55793924fb112cc5d43de2040d35391cd8a4
youngmin <youngmin@ex-em.com>
2021-06-30 12:43:21 +0900

    added incompleted matplotlib practice

--- stdev0617/valuation.py
+++ stdev0617/valuation.py
@@ -1,15 +1,26 @@
 from pykrx import stock
+from matplotlib import pyplot as plt
 
 # type은 day, month, year
 def getDefaultValuation(start_date, end_date, stock_id, type):
     df = stock.get_market_fundamental_by_date(start_date,end_date,stock_id,type)
     print(df)
+    return df
+
+# 미완성. 진행중.
+def drawDataFrame(df):
+
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10,10))
+    axes[0][0].get_childredn()
+
+    plt.plot()
+    plt.show()
 
 def init():
     start_date = '20100101'
     end_date = '20210628'
     stock_id = '005930'
     type = 'y'
-    getDefaultValuation(start_date, end_date, stock_id, type)
+    drawDataFrame(getDefaultValuation(start_date, end_date, stock_id, type))
 
 init()
