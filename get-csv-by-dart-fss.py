3652ba3b0ee6c22af697a9958a59ef8387d33716
oneum20 <63915349+oneum20@users.noreply.github.com>
2021-07-10 20:11:00 +0900

    Update get-csv-by-dart-fss.py
    - support other type (cf, is)

--- get-csv-by-dart-fss.py
+++ get-csv-by-dart-fss.py
@@ -4,6 +4,51 @@ import dart_fss as dart
 
 import pandas as pd
 
+def get_csv_corp_info_by_type(api_key, corp_code, type):    
+    fs = dart.fs.extract(corp_code=corp_code, bgn_de='20200101', fs_tp=[type], report_tp='annual', lang='ko')
+    df = fs[type]
+    
+    data_idx = 4 if type != 'is' else 3
+
+    # drop columns
+    df.drop('concept_id', inplace=True, axis=1, level=1)
+    df.drop('label_en', inplace=True, axis=1, level=1)
+    df.drop('class0', inplace=True, axis=1, level=1)
+    df.drop('class4', inplace=True, axis=1, level=1)
+
+    # manufacturingDataFrame
+    long_clm = df.columns[0][0]
+    df.rename(columns={long_clm:""}, inplace=True) # Rename Long Columns
+    level0 = list(df.columns.get_level_values(0)) # Get 0 level index
+    level1 = list(df.columns.get_level_values(1)) # Get 1 level index
+
+    level0[data_idx:] = [ str(x)[:4] for x in level0[data_idx:]] # years
+    level1[data_idx:] = ['']*len(level1[data_idx:])
+
+    years = level0[data_idx:]
+
+    level = [str(x) + str(y) for x, y in zip(level0, level1)]
+    df.columns = level # Columns
+
+    # add corp_code columns
+    df.insert(0, 'corp_code', corp_code)    
+
+    # Get CSV
+    col = list(df.columns)[:data_idx + 1]
+    print(col)
+    for year in years :
+        print(">> ", year)
+
+        col_tmp = col + [year]
+
+        df_tmp = df[col_tmp]
+        df_tmp.rename(columns={year:"data"}, inplace=True)
+
+        # add year column
+        df_tmp.insert(1, 'year', year)
+
+        df_tmp.to_csv("data/{type}_{year}.csv".format(type = type, year = year), sep=",", na_rep="")
+
 # Set API Key
 print("[RUN] Setting API key...")
 api_key = input("API Key를 입력하세요: ")
@@ -16,38 +61,6 @@ corps = [*corp_list._corp_codes]
 
 # Get Data
 print("[RUN] Getting Data...")
-corp_code = '00126380'
-fs = dart.fs.extract(corp_code=corp_code, bgn_de='20200101', fs_tp=['bs'], report_tp='annual', lang='ko')
-df = fs['bs']
-
-# Convert DataFrame
-## two dimension index -> one dimension index
-long_clm = df.columns[0][0]
-df.rename(columns={long_clm:""}, inplace=True) # Rename Long Columns
-level0 = list(df.columns.get_level_values(0)) # Get 0 level index
-level1 = list(df.columns.get_level_values(1)) # Get 1 level index
-level1[8:] = ['']*len(level1[8:])
-
-level = [x + y for x, y in zip(level0, level1)]
-df.columns = level # Columns
-
-# add corp_code columns
-df.insert(0, 'corp_code', corp_code)
-
-# Config Columns
-col = list(df.columns[:8])
-years = list(df.columns[9:])
-
-# Get CSV
-for year in years :
-    print(">> ", year)
-
-    col_tmp = col + [year]
-
-    df_tmp = df[col_tmp]
-    df_tmp.rename(columns={year:"data"}, inplace=True)
-
-    # add year column
-    df_tmp.insert(1, 'year', year)
-
-    df_tmp.to_csv(year + ".csv", sep=",", na_rep="")
+get_csv_corp_info_by_type(api_key, '00126380', 'bs')
+get_csv_corp_info_by_type(api_key, '00126380', 'cf')
+get_csv_corp_info_by_type(api_key, '00126380', 'is')
